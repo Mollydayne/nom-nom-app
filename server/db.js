565f068edfe15a8e0f4435a9c528c1cee62d86e0
Mollydayne <cl.w.perez@gmail.com>
2025-06-20 13:09:21 +0200

    load dotenv directly in db.js to ensure DATABASE_URL is available

--- server/db.js
+++ server/db.js
@@ -1,15 +1,15 @@
 // server/db.js
-require('dotenv').config();
+require('dotenv').config(); 
+
 const { Pool } = require('pg');
 
-// Ajout temporaire pour débug
 console.log('🔍 DATABASE_URL =', process.env.DATABASE_URL);
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: {
-    rejectUnauthorized: false // nécessaire sur Railway
-  }
+    rejectUnauthorized: false,
+  },
 });
 
 module.exports = pool;
