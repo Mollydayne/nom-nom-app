82c10ed2be64f2b312176544b557676171990096
Mollydayne <cl.w.perez@gmail.com>
2025-06-20 12:14:25 +0200

    add console.log to verify DATABASE_URL is correctly loaded in prod

--- server/db.js
+++ server/db.js
@@ -2,8 +2,11 @@
 require('dotenv').config();
 const { Pool } = require('pg');
 
+// Ajout temporaire pour débug
+console.log('🔍 DATABASE_URL =', process.env.DATABASE_URL);
+
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL, // 
+  connectionString: process.env.DATABASE_URL,
   ssl: {
     rejectUnauthorized: false // nécessaire sur Railway
   }
