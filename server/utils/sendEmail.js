13195830f0c512370426a67bd7e3b1c91aa5e84b
Mollydayne <cl.w.perez@gmail.com>
2025-07-09 16:07:00 +0200

    Fix: add missing sendEmail.js utility for password reset email

---
+++ server/utils/sendEmail.js
@@ -0,0 +1,24 @@
+// utils/sendEmail.js
+
+const nodemailer = require('nodemailer');
+
+const sendEmail = async (to, subject, htmlContent) => {
+  const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: process.env.EMAIL_USER,
+      pass: process.env.EMAIL_PASS,
+    },
+  });
+
+  const mailOptions = {
+    from: `"NomNom 🍱" <${process.env.EMAIL_USER}>`,
+    to,
+    subject,
+    html: htmlContent,
+  };
+
+  await transporter.sendMail(mailOptions);
+};
+
+module.exports = sendEmail;
